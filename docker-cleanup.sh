33f86789e697fc2fc65505f09a377337e6b75e9e
KBuncyShaddai <buncy.shaddai@oneconvergence.com>
2023-03-01 16:15:31 +0530

    docker cleanup

---
+++ docker-cleanup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo docker rm -f $(sudo docker ps -qa)
+sudo docker rmi -f $(sudo docker images -q)
+sudo docker volume rm $(sudo docker volume ls -q)
+#sudo reboot
+sudo rm -rf /etc/ceph \
+       /etc/cni \
+       /etc/kubernetes \
+       /opt/cni \
+       /opt/rke \
+       /run/secrets/kubernetes.io \
+       /run/calico \
+       /run/flannel \
+       /var/lib/calico \
+       /var/lib/etcd \
+       /var/lib/cni \
+       /var/lib/kubelet \
+       /var/lib/rancher/rke/log \
+       /var/log/containers \
+       /var/log/kube-audit \
+       /var/log/pods \
+       /var/run/calico23
+sudo rm -rf /etc/kubernetes/ /var/lib/kubelet/ /var/lib/etcd/
+sudo docker system prune -f
