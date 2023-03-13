c99eba081669f7f9f3375da84fd1335067376e28
KBuncyShaddai <buncy.shaddai@oneconvergence.com>
2023-03-13 11:59:51 +0530

    Added unmounting

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -4,6 +4,7 @@ sudo docker rm -f $(sudo docker ps -qa)
 sudo docker rmi -f $(sudo docker images -q)
 sudo docker volume rm $(sudo docker volume ls -q)
 #sudo reboot
+for mount in $(mount | grep tmpfs | grep '/var/lib/kubelet' | awk '{ print $3 }') /var/lib/kubelet /var/lib/rancher; do sudo umount $mount; done
 sudo rm -rf /etc/ceph \
        /etc/cni \
        /etc/kubernetes \
